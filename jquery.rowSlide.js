bfd53edfee5d5ea8548ade225b0025159d380d8b
Leith Caldwell <leith@codedruids.com>
2015-11-25 15:16:10 +1300

    Use unwrap() when there are child elements to prevent event handlers being destroyed; reformat the whitespace a little

--- jquery.rowSlide.js
+++ jquery.rowSlide.js
@@ -2,20 +2,29 @@
 $.fn.rowSlideUp = function(duration, callback) {
 	if (!duration) duration = 500;
 	if (!callback) callback = function() {};
-	return $(this).find('td,th').wrapInner('<div/>').parent().find('td > div, th > div')
-					.slideUp(duration, function() { 
-						$(this).parent().parent().hide(0, function() { 
-							$(this).find('td > div, th > div').each(function() { $(this).replaceWith($(this).html()); });
-							if (callback && typeof(callback) === "function") callback.call(this); 
-						});  
-					}); 
+	return $(this)
+		.find('td,th').wrapInner('<div/>').end()
+		.find('td > div, th > div').slideUp(duration, function() {
+			$(this).closest('tr').hide(0, function() {
+				$(this).find('td > div, th > div').each(function() {
+					var $children = $(this).children();
+					if ($children.length > 0) { $children.unwrap(); }
+					else { $(this).replaceWith($(this).html()); }
+				});
+				if (callback && typeof(callback) === "function") callback.call(this);
+			});
+		});
 };
 $.fn.rowSlideDown = function(duration, callback) {
 	if (!duration) duration = 500;
 	if (!callback) callback = function() {};
-	return $(this).find('td,th').wrapInner('<div style="display:none;"/>').parent().show(0).find('td > div, th > div')
-					.slideDown(duration, function() { 
-						$(this).replaceWith($(this).html()); 
-						if (callback && typeof(callback) === "function") callback.call(this); 
-					});
+	return $(this)
+		.find('td,th').wrapInner('<div style="display:none;"/>').end()
+		.show(0)
+		.find('td > div, th > div').slideDown(duration, function() {
+			var $children = $(this).children();
+			if ($children.length > 0) { $children.unwrap(); }
+			else { $(this).replaceWith($(this).html()); }
+			if (callback && typeof(callback) === "function") callback.call(this);
+		});
 };
